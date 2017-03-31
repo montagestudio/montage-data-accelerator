fa8fdd0f94dca164177958f1cf405cac8d6fd511
Harold Thetiot <hthetiot@gmail.com>
2017-03-31 11:42:46 -0700

    update http2 bootstrap to dispatch like express

--- server.js
+++ server.js
@@ -2,20 +2,19 @@ var fs = require('fs');
 var path = require('path');
 var http2 = require('http2');
 var Require = require('mr/bootstrap-node');
+global.XMLHttpRequest = require('xhr2');
 
 server = http2.createServer({
-    key: fs.readFileSync(path.join(__dirname, '/test/localhost.key')),
-    cert: fs.readFileSync(path.join(__dirname, '/test/localhost.crt'))
+    key: fs.readFileSync(path.join(__dirname, '/certs/localhost.key')),
+    cert: fs.readFileSync(path.join(__dirname, '/certs/localhost.crt'))
 }, function (request, response) {
-	
 	Require.loadPackage(".").then(function (mr) {
-		return mr.async('run').then(function (result) {
-			response.setHeader('content-type', 'application/json');
-			response.writeHead(200);
-			response.end(JSON.stringify(result));
+		return mr.async('accelerator').then(function (accelerator) {
+			return accelerator.fetchData(request, response);
 		});
 	}).catch(function (err) {
 		console.error(err);
+		console.error(err.stack);
 		response.writeHead(500);
 		response.end(err.message);
 	});
